7e9ef22a7ca26b41f06fe1ff22a29e3a0c312445
Mad_Mike <allistan23@gmail.com>
2021-07-30 00:49:05 +0300

    book-2 auth + delete

--- src/main/java/com/books/controller/BooksController.java
+++ src/main/java/com/books/controller/BooksController.java
@@ -1,14 +1,15 @@
 package com.books.controller;
 
 import com.books.dto.Book;
-import com.books.exceptions.BadRequestException;
 import com.books.exceptions.NotFoundException;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.apache.logging.log4j.util.Strings.isBlank;
+import static com.books.validator.BookValidator.validate;
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("books")
@@ -16,7 +17,7 @@ public class BooksController {
 
     private int increment = 4;
 
-    private final List<Book> books = new ArrayList<>() {{
+    private List<Book> books = new ArrayList<>() {{
         add(new Book(1, "Чистий код", "Роберт Мартін", 4.4f, 350));
         add(new Book(2, "Найбагатший чоловік у Вавилоні", "Джордж Клейсон", 4.8f, 220));
         add(new Book(3, "Сила моменту тепер", "Екгарт Толле", 4.2f, 530));
@@ -34,28 +35,33 @@ public class BooksController {
 
     @PostMapping
     public Book create(@RequestBody Book newBook) {
-        validateBook(newBook);
+        validate(newBook);
         newBook.setId(increment++);
         books.add(newBook);
         return newBook;
     }
 
     @PutMapping("{id}")
-    public Book update(@PathVariable int id, @RequestBody Book bookForUpdate) {
-        validateBook(bookForUpdate);
-        Book existedBook = getBookById(id);
-        existedBook.update(bookForUpdate);
-        return existedBook;
+    public Book update(@PathVariable int id, @RequestBody Book book) {
+        validate(book);
+        Book existingBook = getBookById(id);
+        existingBook.update(book);
+        return existingBook;
     }
 
-    private void validateBook(Book book) {
-        if (isBlank(book.getName()) || isBlank(book.getAuthor()) || book.getPrice() == null) {
-            throw new BadRequestException();
-        }
+    @Secured("ROLE_ADMIN")
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable int id) {
+        Book existingBook = getBookById(id);
+        remove(existingBook.getId());
     }
 
     private Book getBookById(int id) {
         return books.stream().filter(book -> book.getId() == id).findFirst().orElseThrow(NotFoundException::new);
+    }
+
+    private void remove(Integer idRoRemove) {
+        books = books.stream().filter(book -> book.getId() != idRoRemove).collect(toList());
     }
 
 }
